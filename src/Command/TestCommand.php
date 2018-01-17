29998e40e792b4915fef3e1d4dd3410b5ddec161
Ryan Weaver <weaverryan+github@gmail.com>
2018-01-17 15:19:49 -0500

    ...

--- src/Command/TestCommand.php
+++ src/Command/TestCommand.php
@@ -44,7 +44,7 @@ class TestCommand extends Command
             return 1;
         }
 
-        var_dump($config->getChallengeRunnerUrl());
+        var_dump(base64_encode($config->getChallengeRunnerUrl()));
         $this->client = new Client([
             'base_uri' => $config->getChallengeRunnerUrl()
         ]);
